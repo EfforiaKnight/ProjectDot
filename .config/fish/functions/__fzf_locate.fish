69651f2a667913993d693c341c90e164f4256f0a
EfforiaKnight <tualet00@gmail.com>
2017-06-02 10:49:38 +0300

    Replaced HOWDOI to HOW2. Removed 'edit' from fzf locate.

--- .config/fish/functions/__fzf_locate.fish
+++ .config/fish/functions/__fzf_locate.fish
@@ -1,5 +1,10 @@
 function __fzf_locate --description 'Search using locate command with fzf. Edit file with Ctrl+e'
     set -q FZF_LOCATE_COMMAND
     or set -l FZF_LOCATE_COMMAND "command locate /"
-    fish -c "$FZF_LOCATE_COMMAND" | fzf +m --bind "ctrl-e:execute($EDITOR {})+abort" $FZF_LOCATE_OPTS $FZF_DEFAULT_OPTS
+    fish -c "$FZF_LOCATE_COMMAND" | __fzfcmd -m $FZF_DEFAULT_OPTS $FZF_LOCATE_OPTS | while read -l s; set selects $selects $s; end
+    for select in $selects
+        commandline -it -- "\"$select\""
+        commandline -it -- " "
+    end
+    commandline -f repaint
 end
