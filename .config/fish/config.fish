cc5c0624cce680292706db7ee38daa513da4e839
EfforiaKnight <tualet00@gmail.com>
2017-12-15 15:11:56 +0200

    Rearanging long lines

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,23 +20,61 @@ alias rm='echo "Use trash instead of rm."; false'
 # }
 
 # ================ FZF ================ {
-set -x FZF_DEFAULT_COMMAND 'rg --files --smart-case --no-ignore --hidden --follow --no-messages --glob "!.git/*"'
-set -x FZF_DEFAULT_OPTS "--inline-info --reverse --bind 'ctrl-alt-j:down,ctrl-alt-k:up'"
+# ---------------------------------------------------------------------------------
+# FZF Defaults
+# ---------------------------------------------------------------------------------
+set -x FZF_DEFAULT_COMMAND 'rg --files --smart-case --no-ignore --hidden --follow \
+                               --no-messages --glob "!.git/*"'
+
+set -x FZF_DEFAULT_OPTS "--inline-info --reverse \
+                         --bind 'ctrl-alt-j:down,ctrl-alt-k:up'"
 
 set -x FZF_TMUX 1
 set -x FZF_LEGACY_KEYBINDINGS 0
 
-set -x FZF_FIND_FILE_COMMAND 'rg --files --smart-case --no-ignore --hidden --follow --no-messages --glob "!.git/*"'
-set -x FZF_FIND_FILE_OPTS "--preview 'rougify {} 2>/dev/null' --preview-window right:60%:hidden --bind 'ctrl-e:execute(tmux split-window -fh nvim {}),ctrl-o:execute(open {}),alt-n:preview-down,alt-p:preview-up,?:toggle-preview' --history='/home/efforia/.cache/FZF_history'"
 
+# ---------------------------------------------------------------------------------
+# FZF Find command
+# ---------------------------------------------------------------------------------
+set -x FZF_FIND_FILE_COMMAND 'rg --files --smart-case --no-ignore --hidden \
+                                 --follow --no-messages --glob "!.git/*"'
+
+set -x FZF_FIND_FILE_OPTS "--preview 'rougify {} 2>/dev/null' \
+                           --preview-window right:60%:hidden \
+                           --bind 'ctrl-e:execute(tmux split-window -fh nvim {}), \
+                                   ctrl-o:execute(open {}),alt-n:preview-down, \
+                                   alt-p:preview-up,?:toggle-preview' \
+                           --history='/home/efforia/.cache/FZF_history'"
+
+# ---------------------------------------------------------------------------------
+# FZF Historty
+# ---------------------------------------------------------------------------------
 set -x FZF_REVERSE_ISEARCH_OPTS '--history=/home/efforia/.cache/FZF_reverse_history'
 set -x FZF_FIND_AND_EXECUTE_OPTS '--history=/home/efforia/.cache/FZF_reverse_history'
 
+
+# ---------------------------------------------------------------------------------
+# FZF cd command
+# ---------------------------------------------------------------------------------
 set -x FZF_CD_OPTS '--history=/home/efforia/.cache/FZF_cd_history'
 set -x FZF_CD_WITH_HIDDEN_OPTS '--history=/home/efforia/.cache/FZF_cd_history'
 
-set -x FZF_LOCATE_OPTS "--preview 'rougify {} 2>/dev/null' --preview-window right:60%:hidden --bind 'ctrl-e:execute(tmux split-window -fh nvim {}),ctrl-o:execute(open {}),alt-n:preview-down,alt-p:preview-up,?:toggle-preview' --history='/home/efforia/.cache/FZF_history'"
+# ---------------------------------------------------------------------------------
+# FZF Locate command
+# ---------------------------------------------------------------------------------
+set -x FZF_LOCATE_OPTS "--preview 'rougify {} 2>/dev/null' \
+                        --preview-window right:60%:hidden \
+                        --bind 'ctrl-e:execute(tmux split-window -fh nvim {}), \
+                                ctrl-o:execute(open {}), \
+                                alt-n:preview-down, \
+                                alt-p:preview-up, \
+                                ?:toggle-preview' \
+                        --history='/home/efforia/.cache/FZF_history'"
 
+
+# ---------------------------------------------------------------------------------
+# FZF Color Theme
+# ---------------------------------------------------------------------------------
 function gen_fzf_default_opts --description "Solarized theme for fzf"
     set -l color00 '#002b36'
     set -l color01 '#073642'
